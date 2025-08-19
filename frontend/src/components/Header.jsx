c38ff9238360000dad0abf36c864e9b0c6afe8f6
Dana <dmuktubayeva@gmail.com>
2025-08-19 11:30:32 +0500

    init

---
+++ frontend/src/components/Header.jsx
@@ -0,0 +1,39 @@
+import React from "react";
+import { Navbar, Nav, Container, NavDropdown, Badge } from "react-bootstrap";
+import { FaShoppingCart, FaUser } from "react-icons/fa";
+import { LinkContainer } from "react-router-bootstrap";
+import logo from "../assets/logo.png";
+
+const Header = () => {
+  return (
+    <header>
+      <Navbar bg="dark" variant="dark" expand="md" collapseOnSelect>
+        <Container>
+          <Navbar.Brand href="/">
+            <img src={logo} alt="ProShop logo" />
+            Proshop
+          </Navbar.Brand>
+          <Navbar.Toggle aria-controls="basic-navbar-nav" />
+          <Navbar.Collapse id="basic-navbar-nav">
+            <Nav className="ms-auto">
+              <LinkContainer to="/cart">
+                <Nav.Link>
+                  <FaShoppingCart />
+                  CART
+                </Nav.Link>
+              </LinkContainer>
+              <LinkContainer to="/login">
+                <Nav.Link>
+                  <FaUser />
+                  LOGIN
+                </Nav.Link>
+              </LinkContainer>
+            </Nav>
+          </Navbar.Collapse>
+        </Container>
+      </Navbar>
+    </header>
+  );
+};
+
+export default Header;
---
+++ frontend/src/components/Product.jsx
@@ -0,0 +1,33 @@
+import React from "react";
+import { Card } from "react-bootstrap";
+import { Link } from "react-router-dom";
+import Rating from "./Rating";
+
+const Product = ({ product }) => {
+  return (
+    <>
+      <Link to={`/products/${product._id}`}>
+        <Card className="my-3 p-3 rounded">
+          <Card.Img src={product.image} variant="top" />
+
+          <Card.Body>
+            <Card.Title as="div" className="product-title">
+              <strong>{product.name}</strong>
+            </Card.Title>
+
+            <Card.Text as="div">
+              <Rating
+                value={product.rating}
+                text={`${product.numReviews} reviews`}
+              />
+            </Card.Text>
+
+            <Card.Text as="h3">${product.price}</Card.Text>
+          </Card.Body>
+        </Card>
+      </Link>
+    </>
+  );
+};
+
+export default Product;
